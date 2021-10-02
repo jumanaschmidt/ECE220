65a922ec092cf8bb0ddc0df0d43fd15034e37fbe
Jumana Schmidt <jumanas2@illinois.edu>
2021-10-02 15:22:39 -0500

    mp4 complete ignore mp4 file

--- mp4/semiprime.c
+++ mp4/semiprime.c
@@ -1,6 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+    The two errors are the return values in the int is_prime, so I just switched them.
+    to return 1 when there is a semiprime. The second error that you need a break to get out of the for 
+    loop because you don't need to keep incrementing j to find semiprime. Then it lets you increment
+    i. The third error is k = i%j because it needs to be k = i/j since it takes the number from your
+    input and divides it by the counter to then check if it it prime. This follows what a semiprime
+    number is, since it's checking if k and j are both prime and their product is semiprime number
+    i.
+
+    Partner: macraew2
+
+*/
+
 
 /*
  * is_prime: determines whether the provided number is prime or not
@@ -13,10 +26,10 @@ int is_prime(int number)
     if (number == 1) {return 0;}
     for (i = 2; i < number; i++) { //for each number smaller than it
         if (number % i == 0) { //check if the remainder is 0
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
 
@@ -34,9 +47,11 @@ int print_semiprimes(int a, int b)
         for (j = 2; j <= i; j++) {
             if (i%j == 0) {
                 if (is_prime(j)) {
-                    k = i%j;
+                    k = i/j;
                     if (is_prime(k)) {
                         printf("%d ", i);
+                        ret = 1;
+                        break;
                     }
                 }
             }
